6aac7ddd6b7bb636168994333cbe52ce36cafc82
Mauro Servienti <mauro@topics.it>
2016-01-03 17:09:45 +0100

    Puzzle tests, related to #25, related to #22

--- src/RadicalTests/Tests/PuzzleContainerTests.cs
+++ src/RadicalTests/Tests/PuzzleContainerTests.cs
@@ -575,5 +575,33 @@ namespace RadicalTests
 
             Assert.AreEqual(iBar, iFoo);
         }
+
+        class DisposableFoo : IFoo, IDisposable
+        {
+            public Boolean Disposed { get; private set; }
+
+            public void Dispose()
+            {
+                this.Disposed = true;
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("PuzzleContainer")]
+        public void PuzzleContainer_at_dispose_should_dispose_singleton_references()
+        {
+            var entry = EntryBuilder.For<IFoo>()
+                .ImplementedBy<DisposableFoo>()
+                .WithLifestyle(Lifestyle.Singleton);
+
+            var sut = new PuzzleContainer();
+            sut.Register(entry);
+
+            var iFoo = (DisposableFoo)sut.Resolve<IFoo>();
+
+            sut.Dispose();
+
+            Assert.IsTrue(iFoo.Disposed);
+        }
     }
 }
