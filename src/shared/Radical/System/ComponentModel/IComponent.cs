dada63d5a33a2adea4e3e47e4667999e479134e5
Mauro Servienti <mauro@topics.it>
2016-01-10 08:49:33 +0100

    Adding IComponent support

---
+++ src/shared/Radical/System/ComponentModel/IComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.ComponentModel
+{
+    public interface IComponent: IDisposable
+    {
+        event EventHandler Disposed;
+    }
+}
